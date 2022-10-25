0ac2a5880ba30363ac4038ea78ac1d5542c7af18
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-10-25 01:16:04 -0300

    implementacao do carrossel de imagens/posts

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
     $('.menu-toggle').click(function() {
         $('.nav').toggleClass('showing')
     });
+
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+      });
 });
