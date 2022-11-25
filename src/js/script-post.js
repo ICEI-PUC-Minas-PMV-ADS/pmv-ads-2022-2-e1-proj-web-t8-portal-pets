be831f23e390052d51d2e50c40b9234f5901cd9f
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-11-25 01:13:12 -0300

    ajustes na validacao de comentarios

--- src/js/script-post.js
+++ src/js/script-post.js
@@ -19,16 +19,21 @@ $(document).ready(function() {
 });
 
 function addComment(comments) {
-  var addObj = {
-    "name": $("#name").val(),
-    "email": $("#email").val(),
-    "date": today(),
-    "text": $("#text").val()
-  };
-  comments.push(addObj);
-  window.localStorage.commentsData = JSON.stringify(comments);
-  showComments(addObj);
-  cleanParameters();
+  if (validateSubmit()) {
+    var comment = {
+      name: $("#name").val(),
+      email: $("#email").val(),
+      text: $("#text").val(),
+      date: today()
+    };
+    comments.push(comment);
+    window.localStorage.commentsData = JSON.stringify(comments);
+    showComments(comment);
+    cleanParameters();
+  }
+  else {
+    invalidateSubmit();
+  }
 }
 
 function showComments(data) {
@@ -49,4 +54,68 @@ function cleanParameters() {
   $("#name").val("");
   $("#email").val("");
   $("#text").val("");
+}
+
+function validateSubmit() {
+  if (validateName() && validateEmail() && validateText()) {
+    document.getElementById("button-error").innerHTML = "";
+    return true;
+  }
+  return false;
+}
+
+function validateName() {
+  var name = $("#name").val();
+  if (name.length == 0) {
+    invalidateInput("name", "name-error", "* campo obrigatório");
+    return false;
+  } 
+  if (!name.match(/^[a-zA-ZÀ-ÿ]+ [a-zA-ZÀ-ÿ]+$/)) {
+    invalidateInput("name", "name-error", "* nome e sobrenome");
+    return false;
+  }
+    validateInput("name", "name-error");
+    return true;
+}
+
+function validateEmail() {
+  var email = $("#email").val();
+  if (email.length == 0) {
+    invalidateInput("email", "email-error", "* campo obrigatório");
+    return false;
+  }
+  if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+    invalidateInput("email", "email-error", "* email inválido");
+    return false;
+  }
+  validateInput("email", "email-error");
+  return true;
+}
+
+function validateText() {
+  var text = $("#text").val();
+  if (text.length == 0) {
+    invalidateInput("text", "text-error", "* campo obrigatório");
+    return false;
+  }
+  validateInput("text", "text-error");
+  return true;
+}
+
+function invalidateInput(inputId, inputErrorId, inputErrorMessage) {
+  var input = document.getElementById(inputId);
+  input.style.borderColor = "red";
+  input.style.borderWidth = "2px";
+  input.style.borderStyle = "solid";
+  document.getElementById(inputErrorId).innerHTML = inputErrorMessage;
+}
+
+function invalidateSubmit() {
+  document.getElementById("button-error").style.display = "block";
+  document.getElementById("button-error").innerHTML = "* preencha os campos corretamente";
+}
+
+function validateInput(input, inputError) {
+  document.getElementById(input).style.borderStyle = "none";
+  document.getElementById(inputError).innerHTML = "";
 }
